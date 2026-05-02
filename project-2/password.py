800b07555c62c2ade9d2d9cb0d799371bac1ec05
kashishbajpai136 <kashishbajpai325@gmail.com>
2026-05-02 22:18:54 +0530

    Add files via upload

---
+++ project-2/password.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+try:
+    length = int(input("Enter desired password length: "))
+
+    if length <= 0:
+        print("Please enter a valid positive number.")
+    else:
+        password = generate_password(length)
+        print("\nGenerated Password:", password)
+
+except ValueError:
+    print("Invalid input! Please enter a number.")
